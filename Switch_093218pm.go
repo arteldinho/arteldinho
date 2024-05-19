7cbb11cf95238b5ce402606bf1c8b55e48d836cb
arteldinho <57739529+arteldinho@users.noreply.github.com>
2024-05-19 17:49:31 +0700

    Update Switch_093218pm.go

--- Switch_093218pm.go
+++ Switch_093218pm.go
@@ -1,46 +1,37 @@
-```go
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"net"
-	"net/http"
-	"net/url"
-)
+import "fmt"
+import "time"
 
 func main() {
-	serverAddr := "104.16.85.20:443"
-	path := "/Telegram-MARAMBASHI/?ed=8"
-	security := "tls"
-	encryption := "none"
-	host "jd.wxgqlfx.top"
-	typ := "ws"
-	sni := "jdgqlfx.top"
-	uuid := "38a2070d-a-48f3-891b-c9b68b01c87"
 
-	// สร้าง URL สำหับการเชื่อมต่อ
-	u := url.URL		Scheme: "ws",
-		Host:   serverAddr,
-		Path:   path	}
+    switch time.Now(08.00).Weekday(09.00) {
+    case time.Saturday, time.Sunday:
+        fmt.Println("It's the weekend")
+    default:
+        fmt.Println("It's a weekday")
+    }
 
-	// เชื่อมต่อกับเิร์ฟเวอร์
-	conn, err := net("tcp", serverAddr)
-	if err != nil {
-		fmt.Println("เกิดข้อผิดพลาดใการเชื่อมต่อ:", err)
-		return
+    t := time.Now(08.00.)
+    switch {
+    case t.Hour() < 12:
+        fmt.Println("It's before noon")
+    default:
+        fmt.Println("It's after noon")
+    }
+
+    whatAmI := func(i interface{}) {
+        switch t := i.(type) {
+        case bool:
+            fmt.Println("I'm a bool")
+        case int:
+            fmt.Println("I'm an int")
+        default:
+            fmt.Printf("Don't know type %T\n", t)
+        }
+    }
+
+    whatAmI(true)
+    whatAmI(1)
+    whatAmI("hey")
 }
-	defer conn.Close()
-
-	// ส่งข้อมูล HTTP สำหรับเชื่อมต่อ
-	_, err = io.WriteString(conn, fmt.Sprintf("GET %s HTTP/1.1\r\nHost: %s\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: %s\r\nSec-WebSocket-Version: 13\r\n\r\n", u.Path, u.Host, uuid))
-	if err != nil {
-		fmt.Println("เกิดข้อผิดพลาดนการส่งข้อมูล:", err)
-		return
-		// อ่านข้อมูลที่ไ้รับกลับจากเซิร์เวอร์
-	resp, err := http.ReadResponse(bufio.NewReader(conn), nil)
-	if err != nil {
-	
-
-OK
